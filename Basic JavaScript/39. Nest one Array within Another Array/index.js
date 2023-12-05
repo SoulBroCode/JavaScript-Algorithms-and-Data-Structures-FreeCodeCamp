fd71c22b29f158aa66a5cd4a6d3d625c4acf9d00
SoulBroCode <linhhuynh2000@gmail.com>
2023-12-05 20:53:33 +0000

    Basic JavaScript - Step 31-56

---
+++ Basic JavaScript/39. Nest one Array within Another Array/index.js
@@ -0,0 +1,5 @@
+// Only change code below this line
+const myArray = [
+  ["Bulls", 23],
+  ["White Sox", 45],
+];
---
+++ Basic JavaScript/47. Shopping List/index.js
@@ -0,0 +1,7 @@
+const myList = [
+  ["milk", 1],
+  ["juice", 2],
+  ["apple", 3],
+  ["orange", 1],
+  ["lemon", 1],
+];
